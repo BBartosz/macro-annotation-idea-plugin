2ade0d5882c231cbee5a57eaf9bf58717faea0e9
BBartosz <bbartek91@gmail.com>
2016-01-25 02:23:59 +0100

    Add plugin for TalkingAnimalSpell macro annotation

--- src/main/scala/org/jetbrains/example/injector/Injector.scala
+++ src/main/scala/org/jetbrains/example/injector/Injector.scala
@@ -1,21 +1,13 @@
 package org.jetbrains.example.injector
 
-import org.jetbrains.plugins.scala.lang.psi.api.statements.ScValue
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScTypeDefinition}
 import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.SyntheticMembersInjector
-import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
 
-/**
-  * @author Alefas
-  * @since  14/10/15
-  */
 class Injector extends SyntheticMembersInjector {
   override def injectFunctions(source: ScTypeDefinition): Seq[String] = {
-    source.members.flatMap {
-      case v: ScValue if v.hasAnnotation("example.JavaGetter").isDefined =>
-        v.declaredElements.map { td =>
-          s"def get${td.name.capitalize} : ${td.getType(TypingContext.empty).getOrAny.canonicalText} = ???"
-        }
+    source match {
+      case c: ScClass if c.hasAnnotation("TalkingAnimalSpell").isDefined =>
+        Seq(s"def sayHello: Unit = ???")
       case _ => Seq.empty
     }
   }
